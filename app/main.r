313b85a4bb02bdc2710913ac2bef9571a48cb06b
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-26 21:39:00 +0800

    西元2025年02月26日 (週三) 21時39分00秒 CST

--- app/main.r
+++ app/main.r
@@ -1,6 +1,18 @@
-install.packages("webdriver")
+# install.packages("webdriver")
 library(webdriver)
-webdriver::install_phantomjs()
 
 pjs <- run_phantomjs()
 pjs
+
+ses <- Session$new(port = pjs$port)
+
+ses$go("https://guide.michelin.com/tw/zh_TW/restaurants/page/2")
+ses$getUrl()
+
+# 頁面之間暫停
+Sys.sleep(5)
+
+ses$getTitle()
+
+install <- ses$findElement("h1")
+install$getText()
